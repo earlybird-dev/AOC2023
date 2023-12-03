86eb17f13b13d9b712f1374ad7066bb353372082
Ha Nguyen <trungha.dev@gmail.com>
2023-12-03 14:29:10 +1100

    part 2 solution added

--- Day 2/solution.py
+++ Day 2/solution.py
@@ -5,13 +5,27 @@ for colour in bag.keys():
   for i in range(1, numberOfColour+1):
     combination = str(i) + " " +  colour
     validCombinations.append(combination)
-result = 0
+
+sumOfId = 0
+sumOfPower = 0
+
 f = open("input.txt", "r")
 for game in f:
-  [gameID, picks ]= game.split(":")
+  [gameID, picks] = game.split(":")
   id = gameID.split(" ")[1]
   picks = picks.replace(";", ",").replace("\n", "").split(",")
+
+  # Part 1
   pickSet = set([item.strip() for item in picks])
   if pickSet.issubset(validCombinations):
-    result += int(id)
-print(result)
+    sumOfId += int(id)
+
+  # Part 2
+  powerOfGame = 1
+  for colour in bag.keys():
+    maxNumberOfColour = max([int(item.strip().split(" ")[0]) for item in picks if colour in item])
+    powerOfGame *= maxNumberOfColour
+  sumOfPower += powerOfGame
+
+print(sumOfId)
+print(sumOfPower)
